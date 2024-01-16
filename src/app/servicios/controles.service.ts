dadb57a05c4928ed5bd28e9be8628a85dd18566f
wandy141 <wandynl14.com>
2024-01-16 16:37:24 -0400

    ip del router con https

--- src/app/servicios/controles.service.ts
+++ src/app/servicios/controles.service.ts
@@ -14,7 +14,7 @@ import { departamento } from '../modelos/departamento';
 export class ControlesService {
 
   constructor( public http:HttpClient) { }
-  servidor = 'http://192.168.3.1:8000/api/';
+  servidor = 'https://192.168.3.1:8000/api/';
 
 postControl(controltmp:control):Observable<any>{
 return this.http.post<any>(this.servidor + 'control',{
