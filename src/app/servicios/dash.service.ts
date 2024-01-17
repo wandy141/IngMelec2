d85a78f09cc29e3a364b9b9a5276b2d6e9ff51f4
wandy141 <wandynl14.com>
2024-01-17 14:35:13 -0400

    desde el local

--- src/app/servicios/dash.service.ts
+++ src/app/servicios/dash.service.ts
@@ -10,7 +10,7 @@ import { vehiculos } from '../modelos/vehiculos';
 })
 export class DashService {
   constructor(public http: HttpClient) {}
-  servidor = 'https://192.168.3.1:8000/api/';
+  servidor = 'http://192.168.3.253:8000/api/';
 
   insertCombustible(combTemp: combustible): Observable<boolean> {
     return this.http.post<boolean>(this.servidor + 'inserComb', {
